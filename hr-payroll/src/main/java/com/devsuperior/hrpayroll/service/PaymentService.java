c181cb0fc4dcf48c518338da3dfff4c72f5aff30
Fabrício Rodrigues <earthbornshepard@hotmail.com>
2022-07-12 20:56:02 -0300

    feat: hr-worker restTemplate communication

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/service/PaymentService.java
@@ -1,15 +1,36 @@
 package com.devsuperior.hrpayroll.service;
 
 import com.devsuperior.hrpayroll.domain.Payment;
+import com.devsuperior.hrpayroll.domain.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days) {
-        return new Payment(String.valueOf(workerId), 220.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(
+                workerHost + "/workers/{id}",
+                Worker.class,
+                uriVariables
+        );
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
