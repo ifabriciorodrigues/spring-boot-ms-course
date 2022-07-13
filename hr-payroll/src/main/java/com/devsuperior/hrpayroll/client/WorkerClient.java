2ecae013779911be559546e3279a9421912f31d9
Fabrício Rodrigues <earthbornshepard@hotmail.com>
2022-07-12 22:23:24 -0300

    feat: use feign instead of restTemplate

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/client/WorkerClient.java
@@ -0,0 +1,16 @@
+package com.devsuperior.hrpayroll.client;
+
+import com.devsuperior.hrpayroll.domain.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerClient {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+}
