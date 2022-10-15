62115c29ec053f5fd00e929bb6980b46f16cc483
Fabrício Rodrigues <earthbornshepard@hotmail.com>
2022-10-15 17:45:03 -0300

    add: config server users

--- hr-worker/src/main/java/com/hr/worker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/hr/worker/controller/WorkerController.java
@@ -2,10 +2,7 @@ package com.hr.worker.controller;
 
 import com.hr.worker.domain.Worker;
 import com.hr.worker.repository.WorkerRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,19 +17,8 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerController {
 
-    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
-
-    @Value("${test.config}")
-    private String testConfig;
-
     @Autowired
     private WorkerRepository workerRepository;
-
-    @GetMapping(value = "/configs")
-    public ResponseEntity<Void> getConfigs() {
-        logger.info("CONFIG=" + testConfig);
-        return ResponseEntity.noContent().build();
-    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
